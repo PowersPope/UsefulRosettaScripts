f5e91514a6443674c64b39f14a7007a0581a1efa
Andrew Powers <apowers4@uoregon.edu>
2025-05-05 18:11:55 -0700

    <FEAT> A script for filtering MPNN outputs, in a similar manner as our Rosetta scripts way. This has not been tested yet though.

---
+++ scripts/filter_mpnn.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+#
+# @author: Andrew Powers (apowers4@uoregon.edu)
+# @brief: Filter the MPNN outputs based on similar metrics I use for my own Rosetta based designs
+#
+
+# import Packages
+import os, sys
+import glob
+import utils.helper as helpfunc
+import utils.filters as filterfuncs
+import argparse
+
+from pyrosetta import init
+from pyrosetta.rosetta.core import core
+import pyrosetta.rosetta.core.select.residue_selector as rs
+import pyrosett.io as io
+
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser(help_formatter=argparse.ArgumentDefaultsHelpFormatter)
+    p.add_argument("--inpath", type=str, help="path to PDB files")
+    p.add_argument("--silentoutname", type=str, help="Output silent file name")
+    args = p.parse_args()
+
+    init(extra_options="-mute all -out:file:silent_file_struct binary -ex1 -ex2aro -score:weigths ref2015_cart")
+
+    # Setup our silentfile
+    outSilentFile = helpfunc.SilentFileWrite(outname=args.silentoutname)
+
+    # setup our scorefxn
+    scorefxn = helpfunc.init_scorefunction()
+
+    # Grab our files
+    files = glob.glob(os.path.join(args.inpath, "*.pdb"))
+
+    n = 1
+    # Loop through files and run our script through
+    for f in files:
+        pose = io.pose_from_pdb(f)
+
+        # Clone our pose
+        init_pose = pose.clone()
+
+        # get the number of bb-bb hbonds
+        bb_hbonds = filterfunc.determine_internal_bb_hbonds(
+                pose,
+                rs.ChainSelector(1),
+                scorefxn,
+                "prerelax_bb_hbonds",
+                )
+
+        # Setup Movemap
+        mmf = helpfunc.setup_mmf(bb=True, chi=True, bondangles=True, bondlengths=True,
+                                 cartesian=True,)
+        # Relax our output
+        helpfunc.relax_selection(
+                pose,
+                rs.ChainSelection(1),
+                scorefxn,
+                "monomer",
+                True,
+                )
+
+        rmsd = filterfuncs.compare_rmsd_pose(
+                init_pose,
+                pose,
+                rs.ChainSelection(1),
+                rs.ChainSelection(1),
+                "after_relax_rmsd",
+                )
+
+        outSilentFile.generate_plus_add_structure(pose, f"struct_relax_{str(n).zfill(6)}")
+        n+=1
+        
+
+
+
+
+
+
+
+
