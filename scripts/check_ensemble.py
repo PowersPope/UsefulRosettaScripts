071c7768357f99b5d98ad3c1195c76a2e8efd279
PowersPope <apowers4@uoregon.edu>
2026-02-09 14:22:53 -0800

    <FIX> Updated check_ensemble.py, so that it does clustering and removes the scaffolds if they match something. Plus, add in the option to change the rmsd cutoff and to allow energy sorting (if the scaffolds were scored)

--- scripts/check_ensemble.py
+++ scripts/check_ensemble.py
@@ -21,49 +21,56 @@ from pyrosetta.rosetta.core.scoring import ScoreFunction
 
 def silentfile_process(args, 
                        insf: silent.SilentFileData,
-#                        outsf: silent.SilentFileData,
-                       decoy_names: list[str],
                        ) -> list[int]:
-    """Process the outputs for a silent file
+    """Cluster the structures in our SilentFile, to see how many unique clusters
+    are present in our ensemble.
     
     PARAMS
     ------
     :args: The args from our argparse
-    :scorefxn: Our init'd scorefunction for relax/scoring/oversat
-    :outsf: The silentfile of filtered outputs
-    :start_idx: The index to represent the scaffold we are using to compare against
-    :rmsd_mat: RMSD full matrix. This will be returned at the end
-    :decoy_names: rmsd list of decoy names in our SilentDataFile
-
-    RETURNS
-    -------
-    iters and processes the outputs based on our filter workflow
+    :insf: Our passed in SilentDataFile with our structures
     """
+    decoy_names = insf.tags()
     nstructs = len(decoy_names)
+    rmsd_check_list = np.zeros(nstructs, dtype=np.int32)
+    cluster_map = dict()
 
     # list RMSD
-    rmsd_matrix = np.zeros((nstructs, nstructs), dtype=torch.float32)
+#     rmsd_matrix = np.zeros((nstructs, nstructs), dtype=np.float32)
+    skip_scaffolds = list()
 
     # take a pose (i) and (j) 
     pose_i = core.pose.Pose()
     pose_j = core.pose.Pose()
-    for i in range(nstructs):
+    for i in range(1, nstructs):
+        # Init our vec and list for tracking this round
+        if i in skip_scaffolds: continue
+        rmsd_vec = np.zeros(nstructs - len(skip_scaffolds) - 2, dtype=np.float32)
+        scaffolds_accumulated = list()  
+        skip_scaffolds.append(i)
+        scaffold_count = -1
         pose_i.clear()
+
+        # Fill our pose
         silentstruct_i = insf.get_structure(decoy_names[i])
         silentstruct_i.fill_pose(pose_i)
 
-        for j in range(1, nstructs):
+        # Iter through our other availabel structures
+        for j in range(2, nstructs):
+            if j in skip_scaffolds: continue
+            scaffold_count += 1
+
             # init empty poses
             silentstruct_j = insf.get_structure(decoy_names[j])
             silentstruct_j.fill_pose(pose_j)
 
             # Grab the Atom Map to compute RMSD
-            bb_heavy_atommap = helpfuncs.grab_atomid_map(
+            bb_heavy_atommap = helpfunc.grab_atomid_map(
                 pose_reference = pose_i,
                 pose_target = pose_j,
                 residue_anchors = list(range(1, pose_j.size()+1)),
                 target_start_resi = 1,
-                ref_chain = 1,
+                ref_chain = args.peptide_chain,
                 bb_heavy_only = True,
             )
             # Superimpose
@@ -72,12 +79,18 @@ def silentfile_process(args,
                     pose_i,
                     bb_heavy_atommap,
                     )
-            rmsd_matrix[i, j] = rmsd_value
-            rmsd_matrix[j, i] = rmsd_value
+            rmsd_vec[scaffold_count] = rmsd_value
+#             rmsd_matrix[i-1, j-1] = rmsd_value
+#             rmsd_matrix[j-1, i-1] = rmsd_value
+            scaffolds_accumulated.append(j)
             pose_j.clear()
-        rmsd_check = rmsd_matrix[i, :] <= 0.3
-        print("RMSD CHECK <= 0.3:", rmsd_check)
-        print(rmsd_check.sum())
+        rmsd_check = rmsd_vec <= args.rmsd_cutoff
+        cluster_map[decoy_names[i]] = rmsd_check.sum()
+        skip_scaffolds.extend(np.array(scaffolds_accumulated)[rmsd_check].tolist())
+
+    print(f"RMSD Dict of Matches <= {args.rmsd_cutoff}:")
+    print(cluster_map)
+    print("Number of Unique Clusters:", len(cluster_map.keys()))
 
     return 0
 
@@ -85,25 +98,20 @@ def silentfile_process(args,
 if __name__ == "__main__":
     p = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument("--insilent", type=str, default=None, help="path to input silentfile")
-    p.add_argument("--outpath", type=str, default="./", help="path to output our filtered silentfile")
-    p.add_argument("--silentoutname", type=str, help="Output silent file name")
+    p.add_argument("--rmsd-cutoff", type=float, default=0.3, help="How close in RMSD (A), do we want to count as something being in the same cluster.")
     p.add_argument("--peptide-chain", type=int, default=1, help="Which chain is our peptide? Needed for Oversat Filter")
+    p.add_argument("--order_by_energy", "-obe", action="store_true", help="If you scored your structural ensemble, then use this flag to do energy based clustering. \
+            This will segfault if you did not score the outputs!")
     args = p.parse_args()
 
     # Setup our initial Rosetta instance with our presets
-    init(extra_options="-mute all -in:file:fullatom true -out:file:silent_struct_type binary -ex1 -ex2aro -score:weights ref2015_cart")
-
-    # Setup our silentfile
-    outSilentFile = helpfunc.SilentFileWrite(outname=os.path.join(args.outpath,args.silentoutname))
-
-    # setup our scorefxn
-    scorefxn = helpfunc.init_scorefunction()
+    init(extra_options="-mute all -in:file:fullatom true")
 
     # A silent file is passed instead
     sf_data = silent.SilentFileData(silent.SilentFileOptions())
     sf_data.read_file(args.insilent)
-    decoy_names = sf_data.structure_list()
+    if args.order_by_energy: sf_data.order_by_energy()
 
     # filter the outputs in our silentfile
-    silentfile_process(args, sf_data, decoy_names)
+    silentfile_process(args, sf_data)
 
